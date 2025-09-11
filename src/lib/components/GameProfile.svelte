d1c9614294acca736f61af938d69b7b4f8e792c8
Manuel Grgic <manel.gf.95@gmail.com>
2025-09-11 13:28:21 +0200

    add layout toggle

---
+++ src/lib/components/GameProfile.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let game: any;
+</script>
+
+<h1 class="text-2xl font-semibold">{game.name}</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,83 @@
 <script lang="ts">
-    import { ListPlus } from "@lucide/svelte";
-    import Search from "$lib/components/Search.svelte";
+    import { List, ListPlus, LayoutGrid } from "@lucide/svelte";
     import { games } from "$lib/stores/games";
 
-    let showModal = false;
+    import Search from "$lib/components/Search.svelte";
+    import GameProfile from "$lib/components/GameProfile.svelte";
+
+    let addGameModal = false;
+    let gameInfoModal = false;
+    let listLayout = false;
+    let currentGame: any = null;
+
+    function handleGameProfileOpen(game: any) {
+        gameInfoModal = !gameInfoModal;
+        currentGame = game;
+    }
 </script>
 
-<div class="max-w-screen-xl mx-auto border-b-2 border-gray-600 mx-auto p-1 mb-4">
-    <button on:click={() => showModal = true} class="flex items-center hover:bg-white/10 hover:scale-105 transition-transform duration-200 rounded-lg px-2 py-1">
+<div class="max-w-screen-xl mx-auto border-b-2 border-gray-600 mx-auto p-1 mb-4 flex justify-between">
+    <button on:click={() => addGameModal = true} class="flex items-center hover:bg-white/10 hover:scale-105 transition-transform duration-200 rounded-lg px-2 py-1">
         <ListPlus size="18" />
         Add game
     </button>
+    <div class="flex items-center gap-2">
+        <button on:click={() => listLayout = true} class="p-1 rounded-md hover:bg-white/10"> <List size="18" /> </button>
+        <button on:click={() => listLayout = false} class="p-1 rounded-md hover:bg-white/10"> <LayoutGrid size="18" /> </button>
+    </div>
 </div>
 
-<div class="max-w-screen-xl mx-auto flex flex-col md:grid md:grid-cols-2 gap-4">
-    {#each $games as game}
-        <button class="flex flex-col border rounded-lg shadow-sm md:flex-row md:max-w-xl border-gray-700 bg-white/10 hover:bg-white/20 hover:scale-105 transition-transform duration-200">
-            <img class="object-cover w-full h-36 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={game.image.medium_url} alt={`${game.name} cover`}>
-            <div class="flex flex-col justify-between p-4 leading-normal">
-                <h3 class="mb-2 text-lg md:text-2xl font-bold tracking-tight">{game.name}</h3>
-                <p class="mb-2 font-normal text-xs">Release Date: <span>{game.original_release_date}</span></p>
-                <p class="mb-2 text-xs">{game.deck}</p>
-                <ul class="flex flex-wrap gap-2">
-                    {#each game.platforms as p}
-                            <li class="font-normal text-xs px-2 py-1 bg-[#2d7676]/50 rounded-lg">{p.name}</li>
-                    {/each}
-                </ul>
-            </div>
-        </button>
-    {/each}
-</div>
+{#if listLayout}
+    <div class="flex flex-col gap-1 max-w-screen-xl mx-auto">
+        {#each $games as game}
+            <button
+                on:click={() => handleGameProfileOpen(game)}
+                class="flex w-full px-2 py-1 items-center gap-2 rounded-lg bg-white/10 hover:bg-white/20 hover:scale-105 transition-transform duration-200"
+            >
+                <img 
+                    src={game.image.icon_url} alt={`${game.name} cover`}
+                    class="object-cover w-10 h-10"
+                >
+                <h3 class="text-xl font-semibold">{game.name}</h3>
+            </button>
+        {/each}
+    </div>
+{:else}
+    <div class="max-w-screen-xl mx-auto flex flex-col md:grid md:grid-cols-2 gap-4 mb-4">
+        {#each $games as game}
+            <button on:click={() => handleGameProfileOpen(game)} class="flex flex-col border rounded-lg shadow-sm md:flex-row md:max-w-xl border-gray-700 bg-white/10 hover:bg-white/20 hover:scale-105 transition-transform duration-200">
+                <img class="object-cover w-full h-36 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={game.image.medium_url} alt={`${game.name} cover`}>
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h3 class="mb-2 text-lg md:text-2xl font-bold tracking-tight">{game.name}</h3>
+                    <p class="mb-2 font-normal text-xs">Release Date: <span>{game.original_release_date}</span></p>
+                    <p class="mb-2 text-xs">{game.deck}</p>
+                    <ul class="flex flex-wrap gap-2">
+                        {#each game.platforms as p}
+                                <li class="font-normal text-xs px-2 py-1 bg-[#2d7676]/50 rounded-lg">{p.name}</li>
+                        {/each}
+                    </ul>
+                </div>
+            </button>
+        {/each}
+    </div>
+{/if}
 
-{#if showModal}
+{#if addGameModal}
     <div class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
         <div class="bg-[#36454f] rounded-lg shadow-lg p-6 min-w-[350px] min-h-[200px] relative max-h-[50vh] max-w-[75vw] overflow-y-auto">
-            <button class="absolute top-2 right-2 px-2 hover:bg-white/20 mt-2 rounded-md font-black" on:click={() => showModal = false}>
+            <button class="absolute top-2 right-2 px-2 hover:bg-white/20 mt-2 rounded-md font-black" on:click={() => addGameModal = false}>
                 x
             </button>
             <Search />
+        </div>
+    </div>
+{/if}
+
+{#if gameInfoModal}
+    <div class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+        <div class="bg-[#36454f] rounded-lg shadow-lg p-6 min-w-[350px] relative max-h-[50vh] max-w-[75vw] overflow-y-auto">
+            <button class="absolute top-2 right-2 px-2 hover:bg-white/20 mt-2 rounded-md font-black" on:click={() => gameInfoModal = false}>x</button>
+            <GameProfile game={currentGame} />
         </div>
     </div>
 {/if}
