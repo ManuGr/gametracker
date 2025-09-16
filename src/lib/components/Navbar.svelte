e6b7c08d1c9deca82149db513f2665adeb239320
Manuel Grgic <manel.gf.95@gmail.com>
2025-09-16 12:13:49 +0200

    add link to eventual personal website

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,9 +5,9 @@
 <nav class="bg-[#36454f] fixed w-full z-20 start-0 border-b-3 border-gray-600">
     <div class="max-w-screen-xl flex items-center justify-between mx-auto py-4">
         <h1 class="text-xl">Game Tracker</h1>
-        <button class="flex items-center gap-2">
-            <h2>Grunam</h2>
+        <a href="#" class="flex items-center gap-2">
+            <h2>Manuel Figueiredo</h2>
             <img src={logo} alt="Profile pic" class="w-8 h-8 rounded-full">
-        </button>
+        </a>
     </div>
 </nav>
